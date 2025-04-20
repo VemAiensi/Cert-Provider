6cfaa276f7f44d97577757feae3a4d4697154086
Vem Aiensi Marasigan <vem.aiensi@gmail.com>
2025-04-20 09:55:56 +0800

    Added name transformer: Proper Casing

--- server/email.js
+++ server/email.js
@@ -57,20 +57,56 @@ app.post("/send-email", upload.single("image"), async (req, res) => {
 });
 
 app.get("/form-response", async (req, res) => {
+  console.log("Fetching attendees...");
   try {
     const response = await fetch(
       `${process.env.FORM_ENDPOINT}?formId=${req.query.formId}`
     );
 
     if (!response.ok) throw new Error("Failed to fetch attendees");
+
     const data = await response.json();
-    console.log(data);
-    res.json(data);
+    // console.log(data);
+    const clean = data.map((dataItem) => proper(dataItem));
+
+    console.log(clean);
+
+    res.json(clean);
   } catch (error) {
     console.error("Error fetching attendees:", error);
     res.status(500).json({ error: error.message });
   }
 });
+
+//transform email name to proper format.
+function proper(data) {
+  let transformedName = data.name;
+  // console.log(transformedName);
+
+  //make sure Middle Initials have a period
+  if (transformedName.charAt(transformedName.length - 1) !== ".") {
+    transformedName += ".";
+  }
+  //Transform to Proper Casing
+  function capitalize(str) {
+    return str
+      .toLowerCase()
+      .split(" ")
+      .map(function (word) {
+        return word === "ii" || word === "iii" || word === "iv"
+          ? word.toUpperCase()
+          : word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join(" ");
+  }
+
+  transformedName = capitalize(transformedName);
+
+  const newData = { ...data, name: transformedName };
+  // console.log(newData);
+
+  return newData;
+}
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
